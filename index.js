1176ecc6119098a4bfdf9212c025aa2890f4f0c5
John Fajardo <fajardocj@gmail.com>
2019-08-06 09:33:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  const theTime = parseInt(time);
+  if(theTime < 12 && theTime  >= 0){
+    return "Good Morning";
+  } else if(theTime >= 12 && theTime < 17 ){
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  document.getElementById("greeting").innerText = string;
+}
